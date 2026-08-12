81ec09db7314fb834e85f22ec8017e643673919c
Marcus Fernandez <makmak.art@outlook.com>
2026-08-12 01:54:35 -0700

    feat(docker-compose): Configure envrionment variables for backend

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,13 +1,16 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from pydantic import SecretStr
 
 class Settings(BaseSettings):
     app_name: str = "Book Notes"
     debug: bool = False
-    database_url: str = None
+    database_url: str | None = None
+    secret_key: SecretStr
+    jwt_algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
     cors_origins: list[str] = []
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+    # model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
 
 
