c3c0acff98112ac0090b8651ce2899cc6c82e6b0
Marcus Fernandez <makmak.art@outlook.com>
2026-08-12 02:05:10 -0700

    feat(security): Remove backend/app/utils/hashing.py and implement hashing and token generation in security.py

---
+++ backend/app/core/security.py
@@ -0,0 +1,38 @@
+from datetime import UTC, datetime, timedelta
+from typing import Any
+
+import jwt
+from jwt.exceptions import InvalidTokenError
+from pwdlib import PasswordHash
+
+from app.core.config import settings
+
+# ALGORITHM = "HS256"
+
+password_hash = PasswordHash.recommended()
+
+DUMMY_HASH = password_hash.hash("dummypassword1")
+
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return password_hash.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    return password_hash.hash(password)
+
+
+def create_access_token(data: dict[str, Any], expires_delta: timedelta | None = None) -> str: 
+    to_encode = data.copy()
+    expire = datetime.now(UTC) + (
+        expires_delta or timedelta(minutes=settings.access_token_expire_minutes)
+    )
+
+    to_encode["exp"] = expire # or if you had more fields to change, do to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key.get_secret_value(), algorithm=settings.jwt_algorithm)
+    return encoded_jwt
+
+
+def decode_access_token(token: str) -> dict[str, Any]:
+    return jwt.decode(token, settings.secret_key.get_secret_value(), algorithms=[settings.jwt_algorithm])
