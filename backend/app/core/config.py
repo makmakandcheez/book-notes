6fcba87fcbfc5793e80c32b838fa7e89a15bafdf
Marcus Fernandez <makmak.art@outlook.com>
2026-08-23 07:59:26 -0700

    feat(create-refresh-token): Add refresh token expiry value and add function to create a jwt refresh token

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     secret_key: SecretStr
     jwt_algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
+    refresh_token_expire_days: int = 7
     cors_origins: list[str] = []
 
     # model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,5 +1,6 @@
 from datetime import UTC, datetime, timedelta
 from typing import Any
+from uuid import uuid4
 
 import jwt
 from jwt.exceptions import InvalidTokenError
@@ -25,11 +26,35 @@ def get_password_hash(password: str) -> str:
 
 def create_access_token(data: dict[str, Any], expires_delta: timedelta | None = None) -> str: 
     to_encode = data.copy()
+    issued_at = datetime.now(UTC)
     expire = datetime.now(UTC) + (
         expires_delta or timedelta(minutes=settings.access_token_expire_minutes)
     )
+    to_encode.update(
+        {
+            "iat": issued_at,
+            "exp": expire
+        }
+    )
+    # to_encode["exp"] = expire # or if you had more fields to change, do to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key.get_secret_value(), algorithm=settings.jwt_algorithm)
+    return encoded_jwt
 
-    to_encode["exp"] = expire # or if you had more fields to change, do to_encode.update({"exp": expire})
+
+def create_jwt_refresh_token(data: dict[str, any], expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+    issued_at = datetime.now(UTC)
+    expire = datetime.now(UTC) + (
+        expires_delta or timedelta(days=settings.refresh_token_expire_days)
+    )
+    # jti is the JWT id
+    to_encode.update(
+        {
+            "jti": str(uuid4()),
+            "iat": issued_at,
+            "exp": expire
+        }
+    )
     encoded_jwt = jwt.encode(to_encode, settings.secret_key.get_secret_value(), algorithm=settings.jwt_algorithm)
     return encoded_jwt
 
