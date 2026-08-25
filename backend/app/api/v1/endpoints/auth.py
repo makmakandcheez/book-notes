d6f99d9ef533deff54fc9d272afca2405626a473
Marcus Fernandez <makmak.art@outlook.com>
2026-08-24 17:45:08 -0700

    feat(refresh-token-api): Add endpoint for refresh token

--- backend/app/api/v1/endpoints/auth.py
+++ backend/app/api/v1/endpoints/auth.py
@@ -1,14 +1,15 @@
 from typing import Annotated
 
+from jwt import InvalidTokenError
+
 from fastapi import HTTPException, APIRouter, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app.api.v1.dependencies import AuthServiceDep
-
-from app.core.security import create_access_token
+from app.services.auth_service import InvalidCredentialsError, InvalidRefreshTokenError
 
 from app.schemas.user import UserCreate, UserPublic
-from app.schemas.auth import Token
+from app.schemas.auth import TokenResponse, RefreshTokenRequest
 
 
 router = APIRouter(
@@ -27,19 +28,43 @@ async def create_user(data: UserCreate, service: AuthServiceDep) -> UserPublic:
     return UserPublic.model_validate(user)
 
 
-@router.post("/token")
+@router.post("/token", response_model=TokenResponse)
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     auth_service: AuthServiceDep
-) -> Token:
-    user = await auth_service.authenticate_user(form_data.username, form_data.password)
-    if user is None:
+) -> TokenResponse:
+    try:
+        tokens = await auth_service.login(form_data.username, form_data.password)
+    except InvalidCredentialsError as e:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token = create_access_token(
-        data={"sub": str(user.id)}
+            detail=str(e),
+            headers={"WWW-Authenticate": "Bearer"}
+        ) from e
+    response = TokenResponse(
+        access_token=tokens.access_token,
+        refresh_token=tokens.refresh_token,
+        token_type="bearer"
     )
-    return Token(access_token=access_token, token_type="bearer")
+    return TokenResponse.model_validate(response)
+        
+
+@router.post("/refresh-token", response_model=TokenResponse)
+async def refresh(
+    rt: RefreshTokenRequest,
+    auth_service: AuthServiceDep
+) -> TokenResponse:
+    try:
+        tokens = await auth_service.refresh_token(rt.refresh_token)
+    except (InvalidTokenError, InvalidRefreshTokenError) as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=str(e),
+            headers={"WWW-Authenticate": "Bearer"}
+        ) from e
+    response = TokenResponse(
+        access_token=tokens.access_token,
+        refresh_token=tokens.refresh_token,
+        token_type="bearer"
+    )
+    return TokenResponse.model_validate(response)
